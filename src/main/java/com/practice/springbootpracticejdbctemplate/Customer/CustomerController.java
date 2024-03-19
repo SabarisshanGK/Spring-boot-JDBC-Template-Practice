d0f2d9645c7c5040ef71e2cb260127b33b050abd
SabarisshanGK <sabarivisva007@gmail.com>
2024-03-19 21:33:55 +0530

    Updated API Endpoint in application.yml file

--- src/main/java/com/practice/springbootpracticejdbctemplate/Customer/CustomerController.java
+++ src/main/java/com/practice/springbootpracticejdbctemplate/Customer/CustomerController.java
@@ -15,33 +15,33 @@ public class CustomerController {
     }
 
     // GET ALL USERS FROM DATABASE WITH API ENDPOINT: http://localhost:8000/testapi/customers
-    @GetMapping("/testapi/customers")
+    @GetMapping("/customers")
     public List<Customer> getCustomers(){
         return customerService.getAllCustomers();
     }
 
 
     // POST USER TO THE DATABASE WITH API ENDPOINT: http://localhost:8000/testapi/add-customer
-    @PostMapping("/testapi/add-customer")
+    @PostMapping("/add-customer")
     void addCustomer(@RequestBody CustomerRegisterRequest customerRegisterRequest){
         customerService.addCustomer(customerRegisterRequest);
     }
 
     // GET USER BY ID FROM DATABASE WITH API ENDPOINT: http://localhost:8000/testapi/customers/{id}
-    @GetMapping("/testapi/customers/{id}")
+    @GetMapping("/customers/{id}")
     Customer getCustomerByID(@PathVariable("id") Integer id){
         return customerService.getCustomerByID(id);
     }
 
 
     // DELETE USER WITH GIVEN ID FROM DATABASE WITH API ENDPOINT: http://localhost:8000/testapi/delete-customer/{id}
-    @DeleteMapping("/testapi/delete-customer/{id}")
+    @DeleteMapping("/delete-customer/{id}")
     void deleteCustomer(@PathVariable("id") Integer id){
         customerService.deleteCustomer(id);
     }
 
     // UPDATE A CUSTOMER IN DATABASE WITH GIVEN ID THROUGH API ENDPOINT: http://localhost:8000/testapi/update-customer/{id}
-    @PutMapping("/testapi/update-customer/{id}")
+    @PutMapping("/update-customer/{id}")
     void updateCustomer( @RequestBody CustomerUpdateRequest customerUpdateRequest,@PathVariable("id") Integer id){
         customerService.updateCustomer(customerUpdateRequest,id);
     }
